690c71926123a55e8b1ebd306290f5fe1a0fa692
Adam Shumann <ashumann13@gmail.com>
2011-12-04 19:08:50 -0600

    Added README. (Empty for now...)

--- MinecraftLibrary/Packet.cs
+++ MinecraftLibrary/Packet.cs
@@ -6,6 +6,10 @@ using System.IO;
 
 namespace MinecraftLibrary
 {
+    public class slot
+    {
+
+    }
     public abstract class Packet
     {
         public abstract void write(Stream str);
@@ -131,27 +135,27 @@ namespace MinecraftLibrary
         protected void readSlotData(Stream str)
         {
             short[] enchantable = {
- 0x103, //#Flint and steel
- 0x105, //#Bow
- 0x15A, //#Fishing rod
- 0x167, //#Shears
+                0x103, //#Flint and steel
+                0x105, //#Bow
+                0x15A, //#Fishing rod
+                0x167, //#Shears
  
- //#TOOLS
- //#sword, shovel, pickaxe, axe, hoe
- 0x10C, 0x10D, 0x10E, 0x10F, 0x122, //#WOOD
- 0x110, 0x111, 0x112, 0x113, 0x123, //#STONE
- 0x10B, 0x100, 0x101, 0x102, 0x124, //#IRON
- 0x114, 0x115, 0x116, 0x117, 0x125, //#DIAMOND
- 0x11B, 0x11C, 0x11D, 0x11E, 0x126, //#GOLD
+                //#TOOLS
+                //#sword, shovel, pickaxe, axe, hoe
+                0x10C, 0x10D, 0x10E, 0x10F, 0x122, //#WOOD
+                0x110, 0x111, 0x112, 0x113, 0x123, //#STONE
+                0x10B, 0x100, 0x101, 0x102, 0x124, //#IRON
+                0x114, 0x115, 0x116, 0x117, 0x125, //#DIAMOND
+                0x11B, 0x11C, 0x11D, 0x11E, 0x126, //#GOLD
  
- //#ARMOUR
- //#helmet, chestplate, leggings, boots
- 0x12A, 0x12B, 0x12C, 0x12D, //#LEATHER
- 0x12E, 0x12F, 0x130, 0x131, //#CHAIN
- 0x132, 0x133, 0x134, 0x135, //#IRON
- 0x136, 0x137, 0x138, 0x139, //#DIAMOND
- 0x13A, 0x13B, 0x13C, 0x14D  //#GOLD};
-                             };
+                //#ARMOUR
+                //#helmet, chestplate, leggings, boots
+                0x12A, 0x12B, 0x12C, 0x12D, //#LEATHER
+                0x12E, 0x12F, 0x130, 0x131, //#CHAIN
+                0x132, 0x133, 0x134, 0x135, //#IRON
+                0x136, 0x137, 0x138, 0x139, //#DIAMOND
+                0x13A, 0x13B, 0x13C, 0x14D  //#GOLD};
+            };
             if (readShort(str) != -1)
             {
                 readSByte(str);
@@ -1176,6 +1180,5 @@ namespace MinecraftLibrary
             dataString = readString(str);
         }
     }
-
 }
 
---
+++ README.md
@@ -0,0 +1,1 @@
+No readme yet... Just download, build if needed, and run. :)
